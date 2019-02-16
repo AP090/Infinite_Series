5c89fa1e66f2c886d2311457b8f7d0fd2f3295ae
JaPNaA <leonehuang100@gmail.com>
2019-02-16 18:47:49 -0500

    refactor GregoryLeibniz.java
    this refactoring improves performance and uses local variables

--- src/PiCalculator_GregoryLeibniz.java
+++ src/PiCalculator_GregoryLeibniz.java
@@ -4,11 +4,8 @@ import java.util.Scanner;
 
 public class PiCalculator_GregoryLeibniz {
 
-	static Scanner sc = new Scanner(System.in);
-	static int wantedTerms, i;
-	static double denom, sum, piApprox;
-
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
 		System.out.println(
 				"This infinite series is called the Gregory-Leibniz series. It is one of the many infinite series used to calculate pi."
@@ -17,36 +14,31 @@ public class PiCalculator_GregoryLeibniz {
 						+ "\nBy default, this series has one term: -1/3. The 1 is not counted as a term."
 						+ "\nChoose how many terms you want in this series. Unfortunately, this series converges slowly. "
 						+ "\nIt takes 300 terms to calculate pi to two decimal places, so pick a large number!");
-		wantedTerms = sc.nextInt();
+
+		int wantedTerms = sc.nextInt();
 		sc.close();
+
 		piCalc(wantedTerms);
 	}
 
-	public static void piCalc(int wantedTerms) {
-		sum = 1;
-		denom = 3;
-		for (i = 1; i <= wantedTerms; i++) {
+	public static void piCalc(int iterations) {
+		double sum = 1;
+		int denom = 3;
+
+		for (int i = 1; i <= iterations; i++) {
 			if (i % 2 != 0) {
-				sum = piCalcSubTerm(sum, denom);
-			} else if (i % 2 == 0) {
-				sum = piCalcAddTerm(sum, denom);
+				sum -= (1f / denom);
 			} else {
-				System.out.print("Something wierd happened.");
-				main(new String[0]); // This actually isn't necessary, but it'll annoy Leone, so I'll let it stay
+				sum += (1f / denom);
 			}
+
 			denom += 2;
 		}
-		piApprox = sum * 4;
+
+		final double piApprox = sum * 4;
+
 		System.out.println(
-				"The value of pi is calculated to be " + piApprox + " using " + wantedTerms + " terms of the series."
+				"The value of pi is calculated to be " + piApprox + " using " + iterations + " terms of the series."
 						+ "\nFor comparison, this is the value of pi typically used in Java: " + Math.PI + ".");
-	}
-
-	public static double piCalcSubTerm(double sum, double denom) {
-		return sum = sum - (1 / denom);
-	}
-
-	public static double piCalcAddTerm(double sum, double denom) {
-		return sum = sum + (1 / denom);
 	}
 }
