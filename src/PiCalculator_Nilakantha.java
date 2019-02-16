78699f5f04d5e3666ac9d902bbe0b4ebb7654803
JaPNaA <leonehuang100@gmail.com>
2019-02-16 18:56:23 -0500

    refactor this one too

--- src/PiCalculator_Nilakantha.java
+++ src/PiCalculator_Nilakantha.java
@@ -4,11 +4,8 @@ import java.util.Scanner;
 
 public class PiCalculator_Nilakantha {
 
-	static Scanner sc = new Scanner(System.in);
-	static int wantedTerms, i;
-	static double denomA, denomB, denomC, sum, piApprox;
-
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
 		System.out.println(
 				"This infinite series was created by Nilakantha, an Indian mathematician of the 15th and 16th Centuries."
@@ -17,39 +14,29 @@ public class PiCalculator_Nilakantha {
 						+ "\nBy default, this series has one term: 4/(2*3*4). The 3 is not counted as a term."
 						+ "\nChoose how many terms you want in this series. "
 						+ "\nThis series is far superior to Gregory-Leibniz as it converges on pi much faster!");
-		wantedTerms = sc.nextInt();
-		sc.close();
-		piCalc(wantedTerms);
-	}
 
-	public static void piCalc(int wantedTerms) {
-		sum = 3;
-		denomA = 2;
-		denomB = 3;
-		denomC = 4;
-		for (i = 1; i <= wantedTerms; i++) {
-			if (i % 2 != 0) {
-				sum = piCalcAddTerm(sum, denomA, denomB, denomC);
-			} else if (i % 2 == 0) {
-				sum = piCalcSubTerm(sum, denomA, denomB, denomC);
-			} else {
-				System.out.print("Something wierd happened.");
-				main(new String[0]); // This actually isn't necessary, but it'll annoy Leone, so I'll let it stay
-			}
-			denomA += 2;
-			denomB += 2;
-			denomC += 2;
-		}
+		int iterations = sc.nextInt();
+		sc.close();
+
+		double result = piCalc(iterations);
 		System.out.println(
-				"The value of pi is calculated to be " + sum + " using " + wantedTerms + " terms of the series."
+				"The value of pi is calculated to be " + result + " using " + iterations + " terms of the series."
 						+ "\nFor comparison, this is the value of pi typically used in Java: " + Math.PI + ".");
 	}
 
-	public static double piCalcAddTerm(double sum, double denomA, double denomB, double denomC) {
-		return sum = sum + (4 / (denomA * denomB * denomC));
-	}
+	public static double piCalc(int iterations) {
+		double sum = 3;
 
-	public static double piCalcSubTerm(double sum, double denomA, double denomB, double denomC) {
-		return sum = sum - (4 / (denomA * denomB * denomC));
+		for (int i = 1; i <= iterations; i++) {
+			final double denom = i * 2;
+
+			if (i % 2 == 0) {
+				sum -= (4 / (denom * (denom + 1) * (denom + 2)));
+			} else {
+				sum += (4 / (denom * (denom + 1) * (denom + 2)));
+			}
+		}
+
+		return sum;
 	}
 }
