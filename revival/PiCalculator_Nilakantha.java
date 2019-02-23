5f46a88b5f0ffe291209acb33e4582bab33a0d25
BlueMonkeys <forscience090@gmail.com>
2019-02-22 20:20:15 -0500

    Added the updated programs.... in the right spot
    I have updated these programs to be more optimized and generally work better. 
    Changelog:
    1. There are now separate classes for each subset of the program, allowing the user to choose which of the two series' they want to calculate with.
         1A. The launch class is called "Sorting". It gives the user some information about the two series and then asks them to choose which one they want to use. Using that, the postal service forwards that message to the first method of whichever class (series) they chose.
         1B: The first method of each respective class gives the user some more information about the series, and then prompts them to enter how many terms they would like. Forwarding that, the "piCalc" method of each class then proceeds to calculate the value of pi as defined by the series. 
         1C: The value of pi is printed, along with Java's pi and the difference, for comparison.
    2. Try catch statements have been added to idiot proof the system. I'm sure there's some way to break it, but this has added a basic, yet thorough level of protection to the program.
    3. The type of variable for the number of terms in the program has been changed from "int" to "long". This allows the max amount of terms to go from a bit over 2 billion to several quintillion! I have not attempted the latter end of those calculations. Please proceed at your own risk.
    
    Other Statements:
    Initially, the overhaul of the system was much greater. I had gone a little overzealous with using classes, and everything was balkanized into different packages. I had separate classes for the math of each series. However, realizing that the entire class consisted of two single-lined methods, I reintegrated those back into the main class for each series.
    
    Future Plans: 
    Now that I have the basic template down for the creation of series in Java, I can add many more. Methods to calculate pi are just the tip of the iceberg. What about the natural base, e? Or the Taylor Series to calculate trigonometric values? Anything is possible. 
    
    *and that's where one starts flying too close to the sun trying to program mathematical concepts that you barely know about*
    
    
    
    Okay, this took waaaaay too long to write.
    
    
    Same commit description, except in the right spot.

---
+++ revival/PiCalculator_Nilakantha.java
@@ -0,0 +1,52 @@
+package series;
+
+import java.util.Scanner;
+
+public class PiCalculator_Nilakantha {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void infoPanelNK() {
+
+		System.out.println(
+				"\nThis infinite series was created by Nilakantha, an Indian mathematician of the 15th and 16th Centuries.\n"
+						+ "It is one of the many infinite series used to calculate pi.\n\n"
+						+ "This series follows the sequence π = 3 + 4/(2*3*4) - 4/(4*5*6) + 4/(6*7*8)...\n"
+						+ "This series is far superior to Gregory-Leibniz as it converges on pi much faster!\n\n"
+						+ "Choose how many terms of the series you want. By default, 4/(2*3*4) is the first term.");
+		long wantedTerms;
+		while (true) {
+			try {
+				wantedTerms = sc.nextLong();
+				break;
+			} catch (Exception fail) {
+				System.out.println("You entered something stupid. Try again.");
+				sc.nextLine();
+			}
+		}
+		piCalc(wantedTerms);
+	}
+
+	public static void piCalc(long wantedTerms) {
+		double sum = 3;
+		double denomA = 2;
+		double denomB = 3;
+		double denomC = 4;
+		for (int i = 1; i <= wantedTerms; i++) {
+			if (i % 2 != 0) {
+				sum += (4 / (denomA * denomB * denomC));
+			} else if (i % 2 == 0) {
+				sum -= (4 / (denomA * denomB * denomC));
+			} else {
+				System.out.print("Something wierd happened. Sorry.");
+			}
+			denomA += 2;
+			denomB += 2;
+			denomC += 2;
+		}
+		System.out.println("\nThe value of pi is calculated to be " + sum + " using " + wantedTerms
+				+ " terms of the series\n.\n" + "For comparison, this is the value of pi typically used in Java: "
+				+ Math.PI + ".\n" + "This value is " + (Math.abs(sum - Math.PI)) + " off from Java's pi.");
+	}
+
+}
